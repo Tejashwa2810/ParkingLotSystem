9a8fbcfb1538dc754fc5c6c8ef2d16730fed88ca
Tejashwa2810 <tejashwa2810@gmail.com>
2025-06-12 12:29:48 +0530

    Added ParkingLotController

--- src/main/java/com/example/parkinglotsystem/Controller/ParkingLotController.java
+++ src/main/java/com/example/parkinglotsystem/Controller/ParkingLotController.java
@@ -1,9 +1,23 @@
 package com.example.parkinglotsystem.Controller;
 
+import com.example.parkinglotsystem.Models.ParkingLot;
+import com.example.parkinglotsystem.Service.ParkingLotService;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/parking")
 public class ParkingLotController {
+    private ParkingLotService parkingLotService;
+
+    public ParkingLotController(ParkingLotService parkingLotService) {
+        this.parkingLotService = parkingLotService;
+    }
+
+    @PostMapping("/lot")
+    public ParkingLot addParkingLot(@RequestBody ParkingLot parkingLot) {
+        return parkingLotService.getOrCreateSingleton();
+    }
 }
